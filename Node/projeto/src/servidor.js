28cf1d052315df3329b41f1b54e3b5257c57a1b5
victorleite06 <victor.leite2001@gmail.com>
2022-07-15 10:14:40 -0300

    Aula19 Node 	Aula18-19 Node

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,9 +2,22 @@ const porta = 5050
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // converte para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProdutos({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
