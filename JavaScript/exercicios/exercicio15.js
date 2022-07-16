672da7423355e76a68dcb7d7ea0386d403b4d735
victorleite06 <victor.leite2001@gmail.com>
2022-07-16 16:17:43 -0300

    exercicio15

---
+++ JavaScript/exercicios/exercicio15.js
@@ -0,0 +1,29 @@
+const revenda = veiculo => {
+    switch (veiculo) {
+        case 'hatch':
+            console.log('Compra efetuada com sucesso')
+            break;
+
+        case 'sedans':
+            console.log('Tem certeza que não prefere este modelo?')
+            break;
+    
+        case 'motocicletas':
+            console.log('Tem certeza que não prefere este modelo?')
+            break;
+
+        case 'caminhonetes':
+            console.log('Tem certeza que não prefere este modelo?')
+            break;
+
+        default:
+            console.log('Não trabalhamos com este tipo de automóvel aqui')
+            break;
+    }
+}
+
+revenda('hatch')
+revenda('sedans')
+revenda('motocicletas')
+revenda('caminhonetes')
+revenda('avião')
