9f8474ff47067ed4398d9d8c77394e90a65407ab
victorleite06 <victor.leite2001@gmail.com>
2022-07-16 16:07:22 -0300

    exercicio14

---
+++ JavaScript/exercicios/exercicio14.js
@@ -0,0 +1,24 @@
+const frutas = fruta => {
+    switch (fruta) {
+        case 'maçã':
+            console.log('Não vendemos esta fruta aqui')
+            break;
+
+        case 'kiwi':
+            console.log('Estamos com escassez de kiwis')
+            break;
+
+        case 'melancia':
+            console.log('Aqui está, são 3 reais o quilo')
+            break;
+    
+        default:
+            console.log('Erro fruta inválida')
+            break;
+    }
+}
+
+frutas('maçã')
+frutas('kiwi')
+frutas('melancia')
+frutas('limão')
