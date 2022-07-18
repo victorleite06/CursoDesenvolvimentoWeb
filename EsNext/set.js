65aa877a8c594017d5cc337318183b985cfc5c88
victorleite06 <victor.leite2001@gmail.com>
2022-07-18 11:04:07 -0300

    forOf map set

---
+++ EsNext/set.js
@@ -0,0 +1,16 @@
+const times = new Set()
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Corithians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('vasco'))
+console.log(times.has('Vasco'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
