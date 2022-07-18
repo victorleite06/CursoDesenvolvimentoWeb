24808ca63ec482de2ddeb656451015c98cc47ceb
victorleite06 <victor.leite2001@gmail.com>
2022-07-18 10:43:26 -0300

    OperadorSpreadRest taggedTemplates1 taggedTemplates2

---
+++ EsNext/taggedTemplates1.js
@@ -0,0 +1,10 @@
+// tagged templates 
+function tag(partes, ...valores){
+    console.log(partes)
+    console.log(valores)
+    return 'Outra String'
+}
+
+const aluno = 'Gui'
+const situacao = 'Aprovado'
+console.log(tag `${aluno} está ${situacao}.`)
